7abfb038cbf454ab0f485f6213be6332d230fa95
mvacoimbra <mv27.marcosvinicius@gmail.com>
2023-03-02 22:21:31 -0300

    teste de animacoes na sessao wrkFlow no js

--- js/homepage.js
+++ js/homepage.js
@@ -1,25 +1,30 @@
 // SCROLL ANIMATIONS ----------------------------------------------
-const animation_elements = document.querySelectorAll('[data-scrollAni="upNdwn"]');
+const animation_elements = document.querySelectorAll(
+  '[data-scrollAni="upNdwn"]'
+);
 
-const observer = new IntersectionObserver((entries) => {
+const observer = new IntersectionObserver(
+  (entries) => {
     entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('upNdwn__restart--scroll');
-            console.log(entry.target.classList);
-        }
-        // else {
-        //     entry.target.classList.remove('upNdwn__restart--scroll');
-        //     console.log(entry.target.classList);
-        // }
+      if (entry.isIntersecting) {
+        entry.target.classList.add("upNdwn__restart--scroll");
+        console.log(entry.target.classList);
+      }
+      // else {
+      //     entry.target.classList.remove('upNdwn__restart--scroll');
+      //     console.log(entry.target.classList);
+      // }
     });
-}, {
-    threshold: 0.1
-});
+  },
+  {
+    threshold: 0.1,
+  }
+);
 
 for (let i = 0; i < animation_elements.length; i++) {
-    const el = animation_elements[i];
+  const el = animation_elements[i];
 
-    observer.observe(el);
+  observer.observe(el);
 }
 
 // WORKFLOW LIST DRAWER
@@ -27,10 +32,14 @@ for (let i = 0; i < animation_elements.length; i++) {
 const wrkFlwBtn = document.querySelectorAll('[data-wrkflw="button"]');
 const wrkFlwBtnDesc = document.querySelectorAll('[data-wrkflw="desc"]');
 
-function BtnListener() {
-    wrkFlwBtn.forEach(function(index) {
-        index.addEventListener('click');
-    })
+for (let i = 0; i < wrkFlwBtn.length; i++) {
+    wrkFlwBtn[i].addEventListener('click', () => {
+        if (wrkFlwBtnDesc[i].classList.contains('wrkflw__btnDesc--hide')) {
+            wrkFlwBtnDesc[i].classList.remove('wrkflw__btnDesc--hide')
+        } else {
+            wrkFlwBtnDesc[i].classList.add('wrkflw__btnDesc--hide')
+        }
+    });
 }
 
 
