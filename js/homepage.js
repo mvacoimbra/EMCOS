6f09037d0ce6775c1c5a878ea930af78408fa56c
mvacoimbra <mv27.marcosvinicius@gmail.com>
2023-03-10 17:49:21 -0300

    criacao do footer

--- js/homepage.js
+++ js/homepage.js
@@ -46,9 +46,9 @@ for (let i = 0; i < wrkFlwBtn.length; i++) {
 }
 
 // SCREEN TOP FORCED
-window.onbeforeunload = function () {
-  window.scrollTo(0, 0);
-}
+// window.onbeforeunload = function () {
+//   window.scrollTo(0, 0);
+// }
 
 // DARK THEME
 let drkBtn = document.querySelector('[data-theme="btn"]');
